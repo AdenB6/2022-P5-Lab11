55e1b7344d0261e16be465e616bb2cb184a37d9b
15718 <aden.bradshaw@vtsd.com>
2023-01-09 11:45:15 -0500

    1234

--- Lab11avst.java
+++ Lab11avst.java
@@ -6,10 +6,8 @@
 import java.util.Scanner;
 
 
-public class Lab11avst
-{
-    public static void main(String[] args)
-    {
+public class Lab11avst {
+    public static void main(String[] args) {
         // This main method needs additions for the 100 point version.
         Scanner input = new Scanner(System.in);
         final int MAX = 100;
@@ -19,20 +17,38 @@ public class Lab11avst
         displayPrimes(primes);
     }
 
-    public static void computePrimes(boolean primes[])
-    {
+    public static void computePrimes(boolean primes[]) {
         System.out.println("\nCOMPUTING PRIME NUMBERS");
+        // Step 1: Treat all numbers as Prime
+        //Write a Loop that changes all numbers to prime (True) in the array
+        for (int k = 2; k < primes.length; k++) {
+            // Change all the values to true here
+            primes[k] = true;
+        }
 
+
+        for (int k = 2; k < primes.length; k++) {
+            for (int k = 2 * 2; k < primes.length; k += 2) {
+                primes[k] = false;
+            }
+        }
+    }
+        public static void displayPrimes(boolean primes[]) {
+            System.out.println("\n\nPRIMES BETWEEN 1 AND " + primes.length);
+            System.out.println();
+
+            for (int k = 2; k < primes.length; k++) {
+                if (primes[k] == true)
+                    System.out.print(k + "  ");
+            }
+        }
     }
 
-    public static void displayPrimes(boolean primes[])
-    {
-        System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
-        System.out.println();
-
-    }
-
-}
 
 
-
+    // for (int k = 3 * 2; k < primes.length; k += 3) {
+//            primes[k] = false;
+//            }
+//            for (int k = 4 * 2; k < primes.length; k += 4) {
+//            primes[k] = false;
+//            }
